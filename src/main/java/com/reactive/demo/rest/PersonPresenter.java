c0eee2393541e98d9b1514ebdf97410f4b5f58ac
Reza Nur Rochmat <rezanurrochmat@Rezas-MacBook-Pro.local>
2020-06-09 16:37:23 +0700

    Custom Obervables

--- src/main/java/com/reactive/demo/rest/PersonPresenter.java
+++ src/main/java/com/reactive/demo/rest/PersonPresenter.java
@@ -1,11 +1,13 @@
 package com.reactive.demo.rest;
 
 import com.reactive.demo.component.PersonDataObservables;
+import io.reactivex.Observable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -15,7 +17,19 @@ public class PersonPresenter {
     private PersonDataObservables personDataObservables;
 
     @GetMapping("/persons")
-    public Map<String, Object> getAllPersonObservables() {
+    public void getAllPersonObservables() {
+        Map<String, Object> map = new HashMap<>();
+        Observable<String> observablesPerson = personDataObservables.personNamesObservables("Andre");
+
+        try {
+            observablesPerson.subscribe(res -> {
+                String messages = "";
+                messages = res;
+                System.out.println("Subscribe messages : " + messages);
+            });
+        } catch (Exception e) {
+            System.out.println("Error has reached : " + e.getLocalizedMessage());
+        }
 
     }
 }
